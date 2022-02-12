79af8a21f9c9b5cb10bc12e54611f6ccb40251c2
Lautaro López <lopezlautarojoaquin1@gmail.com>
2022-02-12 18:47:43 -0300

    fix(frontend): solve problem with className in Alert component

--- alkemy-client/components/Alert.jsx
+++ alkemy-client/components/Alert.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Alert = ({msg}) => {
 
 	return(
-		<p className="p-2 my-2 bg-red-200 text-red-600 font-bold text-center text-xl">{msg}</p>
+		<p className="p-2 my-2 bg-red-200 text-red-600 font-bold text-center text-xl"><i className="fa-solid fa-circle-exclamation"></i> {msg}</p>
 	)
 
 }
