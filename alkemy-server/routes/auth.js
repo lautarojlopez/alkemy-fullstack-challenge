74c57a30ba45008a5d433a26a10c8cf7d0ca0a0f
Lautaro López <lopezlautarojoaquin1@gmail.com>
2022-02-13 11:11:34 -0300

    fixed(backend): solved problem with router.get('/')

--- alkemy-server/routes/auth.js
+++ alkemy-server/routes/auth.js
@@ -2,11 +2,15 @@ const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/authController')
 const {check} = require('express-validator')
+const auth = require('../middleware/auth')
 
 //Authenticate user
 router.post('/', [
 	check('email', 'Escribe tu e-mail').not().isEmpty(),
 	check('password', 'Escribe tu contraseña').not().isEmpty(),
 ],authController.authenticateUser)
+
+//Get authenticated user
+router.get('/', auth, authController.getAuthenticatedUser)
 
 module.exports = router
--- alkemy-server/routes/users.js
+++ alkemy-server/routes/users.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const usersController = require('../controllers/usersController')
 const {check} = require('express-validator')
-const auth = require('../middleware/auth')
 
 //Create new user
 router.post('/', [
@@ -12,8 +11,5 @@ router.post('/', [
 	check('password').isLength({min: 6}).withMessage('Tu contraseña debe contener al menos 6 caracteres'),
 	check('validate_password', 'Reescribe tu contraseña').not().isEmpty()
 ], usersController.createUser)
-
-//Get authenticated user
-router.get('/', auth, usersController.getAuthenticatedUser)
 
 module.exports = router
