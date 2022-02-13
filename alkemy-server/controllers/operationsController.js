1fd1fe37023d173704e9d8c23a190b8be41de33b
Lautaro López <lopezlautarojoaquin1@gmail.com>
2022-02-13 16:42:11 -0300

    feat(backend): finish Operations API

--- alkemy-server/controllers/operationsController.js
+++ alkemy-server/controllers/operationsController.js
@@ -29,3 +29,97 @@ exports.createOperation = async (req, res) => {
 		})
 	}
 }
+
+//Get authenticated user's operations
+exports.getOperations = async (req, res) => {
+
+	const userId = req.user.id
+	try {
+		//Find operations
+		const results = await Operation.findAll({where: {userId: userId}})
+		//Return operations
+		return res.json(results)
+	} catch (e) {
+		console.log(e)
+		//Return error message
+		return res.status(500).json({
+			msg: "Ha ocurrido un error"
+		})
+	}
+}
+
+//Delete operation
+exports.deleteOperation = async (req, res) => {
+	const operationId = req.params.id
+	try {
+		//Find operation
+		const operation = await Operation.findOne({where: {id: operationId}})
+		//Check if user trying to delete is the same who created the operation
+		if(req.user.id === operation.userId){
+			//If so, delete the operation
+			await operation.destroy()
+			return res.json({
+				msg: "Operacion eliminada con éxito"
+			})
+		}
+		else{
+			return res.status(401).json({
+				msg: "No tienes permiso para realizar esta acción"
+			})
+		}
+
+
+	} catch (e) {
+		console.log(e)
+		return res.status(500).json({
+			msg: "Ha ocurrido un error"
+		})
+	}
+}
+
+
+//Edit operation
+exports.editOperation = async (req, res) => {
+
+	//Check for errors
+	const errors = validationResult(req)
+	if(!errors.isEmpty()){
+	  return res.status(400).json({errors: errors.array()})
+	}
+
+	//Get operation id
+	const operationId = req.params.id
+	//Get new data
+	const {concept, amount, date, category} = req.body
+
+	try {
+		//Find operation
+		const operation = await Operation.findOne({where: {id: operationId}})
+		//Check if user trying to delete is the same who created the operation
+		if(req.user.id === operation.userId){
+			//If so, edit the operation
+			operation.concept = concept
+			operation.amount = amount
+			operation.date = date
+			operation.category = category
+			//Save changes
+			await operation.save()
+			return res.json({
+				msg: "Operacion editada con éxito"
+			})
+		}
+		else{
+			return res.status(401).json({
+				msg: "No tienes permiso para realizar esta acción"
+			})
+		}
+
+
+	} catch (e) {
+		console.log(e)
+		return res.status(500).json({
+			msg: "Ha ocurrido un error"
+		})
+	}
+
+}
--- alkemy-server/routes/operations.js
+++ alkemy-server/routes/operations.js
@@ -14,4 +14,19 @@ router.post('/', [
 	check('type', 'Indica el tipo de operacion').not().isEmpty()
 ], auth,operationsController.createOperation)
 
+//Get authenticated user's operations
+router.get('/', auth, operationsController.getOperations)
+
+//Delete operation
+router.delete('/:id', auth, operationsController.deleteOperation)
+
+//Edit operation
+router.patch('/:id', [
+	//Validation
+	check('concept', 'Escribe una descripción').not().isEmpty(),
+	check('amount', 'Agrega un monto').not().isEmpty(),
+	check('date', 'Añade una fecha').not().isEmpty().isDate().withMessage('Añada una fecha válida'),
+	check('category', 'Añade una categoria').not().isEmpty(),
+], auth, operationsController.editOperation)
+
 module.exports = router
